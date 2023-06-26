748daffebe6c5ec48073d1ce58ffb6374a2092b5
kmpeeduwee <kartsevsb@gmail.com>
2023-06-26 13:46:02 +0200

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,21 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/kartsevsb/development/kartsevsb.dev/app/layout.tsx": [
+      "/Users/kartsevsb/development/kartsevsb.dev/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+      "/Users/kartsevsb/development/kartsevsb.dev/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/local/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"\",\"arguments\":[{\"src\":\"../public/fonts/CalSans-SemiBold.ttf\",\"variable\":\"--font-calsans\"}],\"variableName\":\"calSans\"}",
+      "/Users/kartsevsb/development/kartsevsb.dev/global.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/kartsevsb/development/kartsevsb.dev/app/page": [
+      "/Users/kartsevsb/development/kartsevsb.dev/global.css",
+      "/Users/kartsevsb/development/kartsevsb.dev/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+      "/Users/kartsevsb/development/kartsevsb.dev/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/local/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"\",\"arguments\":[{\"src\":\"../public/fonts/CalSans-SemiBold.ttf\",\"variable\":\"--font-calsans\"}],\"variableName\":\"calSans\"}"
+    ],
+    "/Users/kartsevsb/development/kartsevsb.dev/app/contact/page": [
+      "/Users/kartsevsb/development/kartsevsb.dev/global.css",
+      "/Users/kartsevsb/development/kartsevsb.dev/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+      "/Users/kartsevsb/development/kartsevsb.dev/node_modules/.pnpm/@next+font@13.4.3/node_modules/@next/font/local/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"\",\"arguments\":[{\"src\":\"../public/fonts/CalSans-SemiBold.ttf\",\"variable\":\"--font-calsans\"}],\"variableName\":\"calSans\"}"
+    ]
+  }
+}
